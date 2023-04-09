371b241305a57e64dab257a8043ab3f990789ec3
ZohaibBlockchain <113122909+ZohaibBlockchain@users.noreply.github.com>
2023-04-10 00:59:57 +0400

    v4 Sythe Bot

--- index.js
+++ index.js
@@ -1,36 +1,10 @@
 import express from "express";
 const app = express();
 const port = 443;
-import { tradeFuture, UD, tradeCounter, resetBot, tradeEngine } from "./trade.js";
-
-import bodyParser from "body-parser";
-app.use(bodyParser.json());
+import {_tradeEngine } from "./trade.js";
 
 
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the SYTHE AI Bot");
-});
-
-
-
-app.post("/tradeFuture", (req, res) => {
-  const data = req.body;
-  tradeFuture(data);
-  res.sendStatus(200);
-});
-
-
-
-app.post("/resetBot", (req, res) => {
-  resetBot();
-  res.sendStatus(200);
-});
-
-
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
 
 
 process.on('uncaughtException', function (err) {
@@ -43,17 +17,20 @@ process.on('TypeError', function (err) {
 });
 
 let DisplayCounter = 0;
-function botCore() {
-  if (DisplayCounter > 10) {
-    console.clear();
-    console.log('V1 SYTHE AI Bot +_+ : ');
-    console.log('Current Instruments are: ', UD.length);
-    console.log('SYTHE BOT Health 100 % and total number of trade are: ', tradeCounter);
-    DisplayCounter = 0;
-  }
-  DisplayCounter++;
-  tradeEngine();
+async function botCore() {
+ 
+  await _tradeEngine();
   setTimeout(botCore, 1000);
 }
 
-botCore();
+ botCore();
+
+
+
+//  if (DisplayCounter > 30) {
+//   console.clear();
+//   console.log('V1 SYTHE AI Bot +_+ : ');
+//   console.log('Current Instruments are:');
+//    console.log('SYTHE BOT Health 100 % and total number of trade are: ');
+//   DisplayCounter = 0;
+// }
--- trade.js
+++ trade.js
@@ -1,5 +1,6 @@
 import Binance from "node-binance-api";
 import dotenv from "dotenv";
+import http from 'http';
 dotenv.config();
 
 
@@ -8,180 +9,232 @@ const binance = new Binance().options({
   APISECRET: process.env.API_SECRET,
 });
 
-export let UD = [];
-export let tradeCounter = 0;
-const desireProfitPercentage = 0.5;
+const desireProfitPercentage = 0.1;
 
-class instrument {
-  constructor(symbol, side, tradeAmount, leverage) {
-    this.symbol = symbol;
-    this.side = side;
-    this.tradeAmount = tradeAmount;
-    this.currentTrade = false;
-    this.leverage = leverage;
-    this.flags = ['x', 'x', 'x'];
-    this.price = 0;
+
+let InstrumentRecharge = { BTCUSDT: [{ cooldown: false, buyPrice: 0, sellPrice: 0, ticksLeft: 0 }, { cooldown: false, buyPrice: 0, sellPrice: 0, ticksLeft: 0 }], ETHUSDT: [{ cooldown: false, buyPrice: 0, sellPrice: 0, ticksLeft: 0 }, { cooldown: false, buyPrice: 0, sellPrice: 0, ticksLeft: 0 }], LTCUSDT: [{ cooldown: false, buyPrice: 0, sellPrice: 0, ticksLeft: 0 }, { cooldown: false, buyPrice: 0, sellPrice: 0, ticksLeft: 0 }] }
+
+
+function onCoolDown(symbol, side) {
+
+  if (symbol == 'BTCUSDT' && side == 'long') {
+    return InstrumentRecharge.BTCUSDT[0].cooldown;
+  }
+  if (symbol == 'BTCUSDT' && side == 'short') {
+    return InstrumentRecharge.BTCUSDT[1].cooldown;
   }
 }
 
-export async function tradeFuture(signal) {
-  console.info("signal ", signal);
-  if (await checkConnection()) {
-    let Instrument = getInstrumentData(signal);
-    let instrumentIndex = getInstrumentIndex(Instrument);
-    UD[instrumentIndex].flags[signal.flag] = signal.side;
-    if (Instrument.currentTrade)//Already trade placed...
-    {
-      if (blackFlag(Instrument))//close at any loss
-      {
-        let NewLeverage = setLeverage(signal);
 
-        if (NewLeverage["leverage"] == signal.leverage) {
-          let prvTrade = await settlePreviousTrade(Instrument);
-          if (prvTrade["symbol"] == Instrument.symbol) {//confirmed closed
-            UD[instrumentIndex].currentTrade = false;
-            let newTrade = await CreateNewTrade(signal);
-            if (newTrade["symbol"] == Instrument.symbol) {//successfully created new trade
-              UD[instrumentIndex].side = signal.side;
-              UD[instrumentIndex].tradeAmount = signal.tradeAmount;
-              UD[instrumentIndex].leverage = signal.leverage;
-              UD[instrumentIndex].currentTrade = true;
-              UD[instrumentIndex].price = await getInstrumentPrice(signal.symbol);
-              console.log('Flipped the trade!');
-            } else {//Failed to create new Trade
-              console.log('Failed to create new Trade!');
-            }
-          } else {//failed to close previous trade
-            console.log('Failed to close previous Trade!');
-          }
-        } else {
-          console.log('unable to set leverage!');
-        }
-      }
-      else {
-        console.log('No reason to close the trade');
-      }
+function tradeComplete(symbol, side, buyPrice, sellprice) {
+
+  if (symbol == 'BTCUSDT' && side == 'long') {
+    InstrumentRecharge.BTCUSDT[0].cooldown = true;
+    InstrumentRecharge.BTCUSDT[0].buyPrice = buyPrice;
+    InstrumentRecharge.BTCUSDT[0].sellPrice = sellprice;
+    InstrumentRecharge.BTCUSDT[0].ticksLeft = 300;
+  }
+  if (symbol == 'BTCUSDT' && side == 'short') {
+    InstrumentRecharge.BTCUSDT[1].cooldown = true;
+    InstrumentRecharge.BTCUSDT[1].buyPrice = buyPrice;
+    InstrumentRecharge.BTCUSDT[1].sellPrice = sellprice;
+    InstrumentRecharge.BTCUSDT[0].ticksLeft = 300;
+  }
+}
+
+async function resetCoolDown() {
+  let btcPrice = await getInstrumentPrice('BTCUSDT');
+
+  if (InstrumentRecharge.BTCUSDT[0].cooldown) {
+    (InstrumentRecharge.BTCUSDT[0].ticksLeft > 0) ? InstrumentRecharge.BTCUSDT[0].ticksLeft-- : null;
+    (InstrumentRecharge.BTCUSDT[0].ticksLeft == 0) ? InstrumentRecharge.BTCUSDT[0].cooldown = false : null;
+    let diff = (InstrumentRecharge.BTCUSDT[0].sellPrice - InstrumentRecharge.BTCUSDT[0].buyPrice) / 2;
+    if (btcPrice <= diff + InstrumentRecharge.BTCUSDT[0].buyPrice) {
+      InstrumentRecharge.BTCUSDT[0].cooldown = false
+      InstrumentRecharge.BTCUSDT[0].ticksLeft = 0;
     }
-    else {//New Trade...
-      if (UD[instrumentIndex].flags[0] == signal.side && UD[instrumentIndex].flags[2] == signal.side) {
-        let NewLeverage = await setLeverage(signal);
-        if (NewLeverage["leverage"] == signal.leverage) {
-          let newTrade = await CreateNewTrade(signal);
-          console.log(newTrade);
-          if (newTrade["symbol"] == Instrument.symbol) {//successfully created new trade
-            UD[instrumentIndex].side = signal.side;
-            UD[instrumentIndex].tradeAmount = signal.tradeAmount;
-            UD[instrumentIndex].leverage = signal.leverage;
-            UD[instrumentIndex].currentTrade = true;
-            UD[instrumentIndex].price = await getInstrumentPrice(signal.symbol);
-            console.log('New trade placed and details are ', newTrade);
-          } else {//Failed to create new Trade
-            console.log('Failed to create new Trade!');
-          }
-        } else {
-          console.log('Cannot place new trade due to negative flag or flags!');
-        }
-      } else {
-        console.log('unable to set leverage!');
-      }
+
+  }
+  if (InstrumentRecharge.BTCUSDT[1].cooldown) {
+    (InstrumentRecharge.BTCUSDT[1].ticksLeft > 0) ? InstrumentRecharge.BTCUSDT[1].ticksLeft-- : null;
+    (InstrumentRecharge.BTCUSDT[1].ticksLeft == 0) ? InstrumentRecharge.BTCUSDT[1].cooldown = false : null;
+    let diff = (InstrumentRecharge.BTCUSDT[1].buyPrice - InstrumentRecharge.BTCUSDT[1].sellPrice) / 2;
+    if (btcPrice >= diff + InstrumentRecharge.BTCUSDT[1].buyPrice) {
+      InstrumentRecharge.BTCUSDT[1].cooldown = false
+      InstrumentRecharge.BTCUSDT[1].ticksLeft = 0;
     }
-    console.log(UD[instrumentIndex]);
-  } else {
-    console.log('Unable to connect with exchange!');
   }
 }
 
 
 
-async function setLeverage(instrument) {
+
+
+let engineFlag = true;
+export async function _tradeEngine() {
+  resetCoolDown();
   try {
-    return await binance.futuresLeverage(instrument.symbol, instrument.leverage);
-  } catch (error) {
-    console.log(error);
-  }
-}
+    let totalInstruments = [];
+    await getPositionData().then(async _data => {
+      await getTradeInfo().then(async InsInfo => {
+        const parsedIns = JSON.parse(InsInfo);
+        if (engineFlag) {
 
-async function settlePreviousTrade(instrument) {
-  return new Promise(async (resolve, reject) => {
-    if (instrument.side == "long") {
-      resolve(
-        await binance.futuresMarketSell(instrument.symbol, instrument.tradeAmount)
-      );
-    } else {
-      resolve(
-        await binance.futuresMarketBuy(instrument.symbol, instrument.tradeAmount)
-      );
-    }
-  });
-}
+          _data.positions.forEach(async instruments => {
+            console.log('------Positions Block----')
+            totalInstruments.push(instruments);
+            let totalFee = getFees({ tradeAmount: instruments.positionAmt, price: instruments.entryPrice });
+            let side = getType(instruments.positionAmt);
+            let desireProfit = await checkDesireProfit({ side: side, tradeAmount: Math.abs(instruments.positionAmt), leverage: instruments.leverage, markPrice: instruments.markPrice, price: instruments.entryPrice }, totalFee);
+            console.log('PNL: ', desireProfit);
+            if (desireProfit.profitable) {//if true then close the trade...
+              engineFlag = false;
 
-async function CreateNewTrade(Instrument) {
-  console.log('CNT ', Instrument);
-  return new Promise(async (resolve, reject) => {
-    if (Instrument.side == "long") {
-      resolve(
-        await binance.futuresMarketBuy(Instrument.symbol, Instrument.tradeAmount)
-      );
-    } else {
-      resolve(
-        await binance.futuresMarketSell(Instrument.symbol, Instrument.tradeAmount)
-      );
-    }
-  });
-}
+              let prvTrade = await settlePreviousTrade({ side: side, tradeAmount: Math.abs(instruments.positionAmt), symbol: instruments.symbol });
+              if (prvTrade["symbol"] == instruments.symbol) {//confirmed closed
+                console.log('The trade resulted in a profit.!')
+                tradeComplete(instruments.symbol, side, instruments.entryPrice, instruments.markPrice);  //Now update that we have completed the trade
+                console.log('Profit: ', desireProfit);
+                engineFlag = true;
+              }
+              else {
+                console.log('The previous trade could not be closed!');
+                engineFlag = true;
+              }
+            }
+            else {
+              //BlackFlag
+              let signalInstrument;
+              parsedIns.forEach(element => {
+                if (element.symbol == instruments.symbol)
+                  signalInstrument = element;
+              });
+
+              let signalSide = getSide(signalInstrument.flags);
+              console.log(signalSide.res, '--X');
+
+              if (signalSide.value != undefined) {
+
+                if (blackFlag(side, signalSide.value) && Math.abs(signalSide.res) > 2) {
+                  engineFlag = false;
+                  let prvTrade = await settlePreviousTrade({ side: side, tradeAmount: Math.abs(instruments.positionAmt), symbol: instruments.symbol });
+                  if (prvTrade["symbol"] == instruments.symbol) {//confirmed closed
+                    console.log('The trade resulted in a loss.!')
+                    engineFlag = true;
+                  }
+                  else {
+                    console.log('The previous trade could not be closed!');
+                    engineFlag = true;
+                    return;
+                  }
+                }
+
+                else {
+                  console.log('The profit margin is not sufficient!');
+                }
+              } else {
+                console.log('balanced flags detected--');
+              }
+            }
+
+          });
+          console.log('------Trade starting Block----')
 
 
-export async function close(signal) {
-  if (userData.currentTrade) {
-    settlePreviousTrade();
-    userData.currentTrade = false;
-  } else {
-    console.log('No Trade to close!');
-  }
-}
+          //------Trade starting Area
+          console.log(totalInstruments.length);
+          for (let i = 0; i < parsedIns.length; i++) {
+            let exits = false;
+            for (let u = 0; u < totalInstruments.length; u++) {
+              if (parsedIns[i].symbol == totalInstruments[u].symbol) {
+                exits = true;
+              }
+            }
 
+            if (exits) {
+              //Do nothing
+              console.log('Already trade placed!');
+              console.log('------End Trade starting Block----')
+            } else {
+              let side = getSide(parsedIns[i].flags);
 
+              if (side.value != undefined) {
 
-function getInstrumentData(request) {
-  for (let i = 0; i < UD.length; i++) {
+                if (!onCoolDown(parsedIns[i].symbol, side)) {
+                  let price = await getInstrumentPrice(parsedIns[i].symbol);
+                  let positionAmt = parsedIns[i].positionAmt;
+                  let leverageAmt = parsedIns[i].leverageAmt;
+                  let tradeAmt = ((positionAmt * leverageAmt) / price).toFixed(3);
+                  let _setLeverage = await setLeverage({ symbol: parsedIns[i].symbol, leverage: leverageAmt });
+                  if (_setLeverage["leverage"] == leverageAmt) {
+                    let newTrade = await CreateNewTrade({ side: side.value, tradeAmount: tradeAmt, symbol: parsedIns[i].symbol });
+                    engineFlag = false;
+                    console.log(newTrade);
+                    if (newTrade["symbol"] == parsedIns[i].symbol) {//successfully created new trade
+                      engineFlag = true;
+                    } else {
+                      throw ('unable to place trade');
+                    }
+                  } else {
+                    throw ('unable to set leverage');
+                  }
+                } else {
 
-    if (UD[i].symbol == request.symbol) {
-      return UD[i];
-    }
-  }
-  console.log(request);
-  let newInstrument = new instrument(request.symbol, request.side, request.tradeAmount, request.leverage);
-  UD.push(newInstrument);
-  return newInstrument;
-}
-
-
-function getInstrumentIndex(request) {
-  for (let i = 0; i < UD.length; i++) {
-    if (UD[i].symbol == request.symbol) {
-      return i;
-    }
-  }
-  return undefined;
-}
-
-export function resetBot() {
-  UD = [];
-}
-
-async function checkConnection() {
-  return new Promise(async (resolve, reject) => {
-    binance.prevDay("BTCUSDT", (error, prevDay) => {
-      if (error) {
-        console.error(error);
-        reject(false);
-      } else {
-        console.log('Connection checked 100%');
-        resolve(true);
-      }
+                }
+              }
+              else {
+                console.log('balanced flags detected');
+              }
+            }
+          }
+        } else {
+          console.log('Process Pending!');
+        }
+      });
     });
+  }
+  catch (error) {
+    console.log(error);
+    engineFlag = true;
+  }
+}
+
+
+
+
+
+async function getTradeInfo() {
+  return new Promise(async (resolve, reject) => {
+    const options = {
+      hostname: '3.10.246.161',
+      port: 80,
+      path: '/getsignals',
+      method: 'GET'
+    };
+
+    const req = http.request(options, res => {
+      let data = '';
+      res.on('data', chunk => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        resolve(data);
+      });
+    });
+
+    req.on('error', error => {
+      reject(error);
+    });
+
+    req.end();
   });
 }
+
+
+
+
+
+
 
 async function getInstrumentPrice(symbol) {
   return new Promise(async (resolve, reject) => {
@@ -197,8 +250,58 @@ async function getInstrumentPrice(symbol) {
 }
 
 
+
+
+
+
+async function setLeverage(instrument) {
+  try {
+    return await binance.futuresLeverage(instrument.symbol, instrument.leverage);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+async function settlePreviousTrade(instrument) {
+  return new Promise(async (resolve, reject) => {
+    if (instrument.side == "long") {
+      resolve(
+        await binance.futuresMarketSell(instrument.symbol, instrument.tradeAmount)
+      );
+    } else {
+      resolve(
+        await binance.futuresMarketBuy(instrument.symbol, instrument.tradeAmount)
+      );
+    }
+  });
+}
+
+
+async function CreateNewTrade(Instrument) {
+  return new Promise(async (resolve, reject) => {
+    console.log(Instrument);
+    if (Instrument.side == "long") {
+      resolve(
+        await binance.futuresMarketBuy(Instrument.symbol, Instrument.tradeAmount)
+      );
+    } else if (Instrument.side == "short") {
+      resolve(
+        await binance.futuresMarketSell(Instrument.symbol, Instrument.tradeAmount)
+      );
+    }
+    else {
+      console.log('Unable to detect right weight')
+      reject(false);
+    }
+  });
+}
+
+
+
 function getFees(instrument) {
-  const tradeAmount = instrument.tradeAmount; // Example trade amount in BTC
+  console.log(instrument);
+  const tradeAmount = Math.abs(instrument.tradeAmount); // Example trade amount in BTC
   const takerFeeRate = 0.0004; // Taker fee 
   const usdtRate = instrument.price; // Example BTC/USDT exchange rate
   let fee = tradeAmount * takerFeeRate;
@@ -207,9 +310,11 @@ function getFees(instrument) {
 }
 
 
+
 async function checkDesireProfit(instrument, fee) {
 
-  let getCurrentPrice = await getInstrumentPrice(instrument.symbol);
+
+  let getCurrentPrice = instrument.markPrice;
   let orignalAmount = (getCurrentPrice * instrument.tradeAmount) / instrument.leverage;
   if (instrument.side == 'long' && instrument.price > 0) {
     let pnl = (((getCurrentPrice - instrument.price) * instrument.tradeAmount) - fee);
@@ -246,58 +351,108 @@ async function checkDesireProfit(instrument, fee) {
 
 
 
-
-function blackFlag(Instrument) {
-  if (Instrument.side == 'long') {
-    if (Instrument.flags[0] == 'short', Instrument.flags[1] == 'short', Instrument.flags[2] == 'short') {
-      return true;
-    }
-    else {
-      return false;
-    }
-  } else if (Instrument.side == 'short') {
-    if (Instrument.flags[0] == 'long', Instrument.flags[1] == 'long', Instrument.flags[2] == 'long') {
-      return true;
-    }
-    else {
-      return false;
-    }
-  } else {
-    console.log('Unexpected error!');
+function blackFlag(side, flagSide) {
+  if (side == flagSide) {
     return false;
   }
-}
-
-
-
-function cleanInstrument(index) {
-  UD.splice(index, 1);
-}
-
-
-export async function tradeEngine() {
-  tradeCounter = 0;
-  for (let i = 0; i < UD.length; i++) {
-    if (UD[i].currentTrade) {
-      tradeCounter++;
-      let totalFee = getFees(UD[i]);
-      let desireProfit = await checkDesireProfit(UD[i], totalFee);
-      console.log('SYMBOL: ', UD[i].symbol, ' CURRENT PNL: ', desireProfit.pnl, ' in percentage:', desireProfit.profitPercentage, '% Fee ', totalFee, ' leverage: ', UD[i].leverage);
-      if (desireProfit.profitable) {//if true then close the trade...
-        UD[i].currentTrade = false;
-        let prvTrade = await settlePreviousTrade(UD[i]);
-        if (prvTrade["symbol"] == UD[i].symbol) {//confirmed closed
-          console.log('The trade resulted in a profit.!')
-        }
-        else {
-          console.log('The previous trade could not be closed!');
-          UD[i].currentTrade = true;
-        }
-      } else {
-        console.log('The profit margin is not sufficient!');
-      }
-    } else {
-      console.log('There are currently no trade in the queue.');
-    }
+  else {
+    return true;
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function getInstrumentDetail(symbol) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let position_data = await binance.futuresPositionRisk(), markets = Object.keys(position_data);
+      position_data.forEach(instrument => {
+        if (instrument.symbol == symbol)
+          resolve(instrument);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+
+}
+
+
+
+
+async function getPositionData() {
+  let position_data = await binance.futuresPositionRisk(), markets = Object.keys(position_data);
+  let Positions = [];
+  let counter = 0;
+  position_data.forEach(element => {
+    if (element.positionAmt != 0) {
+      Positions.push(element);
+      counter++;
+    }
+  });
+  return { positions: Positions, counter: counter };
+}
+
+
+
+
+
+
+
+
+function getType(value) {
+  if (value < 0) {
+    return "short";
+  } else {
+    return "long";
+  }
+}
+
+
+
+//weights
+//flag 0 = 1,flag 1 = 2,flag 2 = 2 ,flag 3 = 2,flag 4 = 3
+//long +
+//short -
+
+
+function getSide(flags) {
+
+  let flag0 = (flags[0] == 'long' ? 1 : -1);
+  let flag1 = (flags[1] == 'long' ? 2 : -2);
+  let flag2 = (flags[2] == 'long' ? 2 : -2);
+  let flag3 = (flags[3] == 'long' ? 2 : -2);
+  let flag4 = (flags[4] == 'long' ? 3 : -3);
+  let res = flag0 + flag1 + flag2 + flag3 + flag4;
+  if (res == 0) {
+    return { value: undefined, res };
+  }
+  else if (res > 0) {
+    return { value: 'long', res };
+  }
+  else if (res < 0) {
+    return { value: 'short', res };
+  }
+}
+// let x = ['long','long','short','short','long'];
+// console.log(getSide(x));
